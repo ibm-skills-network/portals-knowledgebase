6458f9eca4a09b80da74182ea3754aac47765be5
Michael Roudnitski <mroudnitski@ibm.com>
2025-02-18 15:57:11 -0500

    fix: editUrl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -44,10 +44,7 @@ const config = {
         docs: {
           routeBasePath: '/',
           sidebarPath: './sidebars.js',
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          editUrl: 'https://github.com/ibm-skills-network/portals-knowledgebase/tree/main',
         },
         blog: {
           showReadingTime: true,
