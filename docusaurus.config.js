5f766941b10a876da3195496195b5bd17d786bfd
LaeekAhmed <laeek385@gmail.com>
2025-04-24 11:21:26 -0400

    refactor: update config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,21 +1,40 @@
 // @ts-check
-// Note: type annotations allow type checking and IDEs autocompletion
+// `@type` JSDoc annotations allow editor autocompletion and type checking
+// (when paired with `@ts-check`).
+// There are various equivalent ways to declare your Docusaurus config.
+// See: https://docusaurus.io/docs/api/docusaurus-config
 
-import { themes } from 'prism-react-renderer';
-const lightCodeTheme = themes.github;
-const darkCodeTheme = themes.dracula;
+import { themes as prismThemes } from 'prism-react-renderer';
+
+// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Portals Knowledgebase',
-  tagline: 'Documentation for Portals',
-  url: 'https://portals.skills.network',
-  baseUrl: '/docs/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  tagline: 'Documentation for Portal learners',
   favicon: 'img/favicon.ico',
+
+  // Set the production url of your site here
+  url: 'https://portals.skills.network',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/docs/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
   organizationName: 'ibm-skills-network', // Usually your GitHub org/user name.
   projectName: 'portals-knowledgebase', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
 
   presets: [
     [
@@ -24,13 +43,12 @@ const config = {
       ({
         docs: {
           routeBasePath: '/',
-          sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
+          sidebarPath: './sidebars.js',
           editUrl: 'https://github.com/ibm-skills-network/portals-knowledgebase/tree/main',
         },
         blog: false,
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: './src/css/custom.css',
         },
       }),
     ],
@@ -58,8 +76,8 @@ const config = {
         copyright: `Copyright © ${new Date().getFullYear()} IBM Skills Network. Built with Docusaurus.`,
       },
       prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
+        theme: prismThemes.github,
+        darkTheme: prismThemes.dracula,
       },
     }),
 };
